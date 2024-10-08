1a763390434467e7306e5a0fcf0090732510513d
Taseem Molique <taseenm100@gmail.com>
2024-10-08 10:50:14 -0400

    DiceRoller

---
+++ src/main/java/com/pluralsight/Dice.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Dice {
+
+    public int roll() {
+        int minValue = 1;
+        int maxValue = 6;
+        return (int)(Math.random()* maxValue) + minValue;
+    }
+
+}
---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args){
+
+        Dice dice = new Dice();
+
+        int roll1, roll2;
+        int twoCounter = 0, fourCounter = 0, sixCounter = 0, sevenCounter = 0;
+
+        for (int i = 0; i < 20; i++ ){
+            roll1 = dice.roll();
+            roll2 = dice.roll();
+
+            System.out.printf("Roll %d: %d - %d Sum: %d%n", i + 1, roll1, roll2, roll1 + roll2);
+
+            int sum = roll1 + roll2;
+
+            if (sum == 2){
+                twoCounter++;
+            } else if (sum == 4){
+                fourCounter++;
+            } else if (sum == 6){
+                sixCounter++;
+            } else if (sum == 7){
+                sevenCounter++;
+            }
+        }
+
+        System.out.println("The number of times 2 was rolled: " + twoCounter);
+        System.out.println("The number of times 4 was rolled: " + fourCounter);
+        System.out.println("The number of times 6 was rolled: " + sixCounter);
+        System.out.println("The number of times 7 was rolled: " + sevenCounter);
+
+
+    }
+
+
+
+
+
+
+}
